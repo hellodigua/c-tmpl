ff8e5b0eb96ea491fdb9b84ef2502fbd37632b81
鲍浙军 <razor.bao@dbappsecurity.com.cn>
2017-10-31 09:17:04 +0800

    新增: 引入element-ui

--- template/src/main.js
+++ template/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 {{#element-ui}}
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
 {{/element-ui}}
 
 /* eslint-disable no-new */
